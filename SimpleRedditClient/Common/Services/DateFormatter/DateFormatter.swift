9409aee516ce5f3ffb665bb923d38eee51d09613
Alexander Tkachenko <atkachenko@megogo.net>
2017-09-18 23:00:26 +0300

    Improve date formatter performance

--- SimpleRedditClient/Common/Services/DateFormatter/DateFormatter.swift
+++ SimpleRedditClient/Common/Services/DateFormatter/DateFormatter.swift
@@ -5,11 +5,15 @@
 import Foundation
 
 final class DateFormatter {
-    func formattedDate(date: Date) -> String {
+    private lazy var formatter: DateComponentsFormatter = {
         let formatter = DateComponentsFormatter()
         formatter.unitsStyle = .full
         formatter.maximumUnitCount = 1
         formatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
+        return formatter
+    }()
+
+    func formattedDate(date: Date) -> String {
         let now = Date()
         guard let timeString = formatter.string(from: date, to: now) else {
             return ""
